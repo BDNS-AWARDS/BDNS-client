5153506d9d6a5c0ed98b7e0a9d42efdd6d8791eb
earl9rey <earl9rey@naver.com>
2023-11-03 16:49:11 +0900

    [성하]push 오류 수정(11/3)

--- myawards/src/components/sample.jsx
+++ myawards/src/components/sample.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const sample = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div>
+      <h1>오류 수정.. 제발 </h1>
+    </div>
+  );
 };
 
 export default sample;
