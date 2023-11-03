54113f323642f3cfbb5a13a68796a2dec89e1f52
Cho Sungha <earl9rey@naver.com>
2023-11-03 15:48:30 +0900

    [성하]sample.jsx 수정(11/3)

--- myawards/src/components/sample.jsx
+++ myawards/src/components/sample.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const sample = () => {
-    return (
-        <div>
-            <h3>안녕하세요~~</h3>
-        </div>
-    );
+  return (
+    <div>
+      <h3>안녕하세요~~!!!!!</h3>
+    </div>
+  );
 };
 
 export default sample;
