ac5d60bd63a9ceb6ee223bcc938bfaf65924e012
idkwtdoob <rkdwndms333@naver.com>
2023-11-06 00:08:38 +0900

    [주은]SetProfile.js수정 및 checkbutton,modalcheck컴포넌트 추가(11/6)

---
+++ myawards/src/components/StartButton.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledButton=styled.button`
+  position: fixed;
+  margin-top:20px;
+  margin-left:40px;
+  border: none;
+  background:none;
+`;
+
+const Image=styled.img`
+  width: 352px;
+  height: 61px;
+`;
+
+const StartButton = ({ isEnabled, onClick }) => {
+  const buttonImage = isEnabled ? "images/start_on.png" : "images/start_off.png";
+
+  return (
+    <StyledButton onClick={onClick}>
+      <Image src={buttonImage} alt="Start" />
+    </StyledButton>
+  );
+};
+
+export default StartButton;
