f35a5bf85657a03b3b68c545779e37d61476d2a5
idkwtdoob <rkdwndms333@naver.com>
2023-11-11 11:49:18 +0900

    [주은]phototomodal추가 및 테스트(11/11)

--- myawards/src/components/PhotoModal.jsx
+++ myawards/src/components/PhotoModal.jsx
@@ -1,7 +1,62 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import styled from "styled-components";
 
-const PhotoModal = () => {
-  return <div></div>;
-};
+const ModalBox = styled.div`
+position: fixed;
+top: 50%;
+left: 50%;
+transform: translate(-50%, -50%);
+background: rgba(0, 0, 0, 0.7);
+z-index: 999;
+display: flex;
+align-items: center;
+justify-content: center;
+width: 100%;
+height: 100%;
+`;
+
+const Modalcontent = styled.div`
+  background: white;
+  border-radius: 10px;
+  padding: 20px;
+  text-align: center;
+`;
+
+const ModalImage = styled.img`
+  width:330px;
+  height:172px;
+`
+
+const PhotoModal = ({show}) => 
+  const [isVisible, setIsVisible] = useState(false);
+  
+  useEffect(()=>{
+    setIsVisible(show);
+
+    if (show) {
+      const timer = setTimeout(()=>{
+        setIsVisible(false);
+      }, 2000);
+
+       // 컴포넌트가 언마운트되면 타이머를 클리어합니다.
+       return () => clearTimeout(timer);
+      }
+    }, [show]);
+  
+    if (!isVisible) {
+      return null;
+    }
+  
+    return (
+      <ModalBox>
+        <Modalcontent>
+          <Modalimage src="images/imagelimitmodal.png" alt="photolimitmodal" />
+        </Modalcontent>
+      </ModalBox>
+    );
+  };
+
 
 export default PhotoModal;
+
+
